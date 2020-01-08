257e4447c23e6c594888076483729b57bd197fcf
Pawel Rolbiecki <pawel.rolbiecki@gmail.com>
2020-01-08 22:21:02 +0100

    Adding downloading single search field from list

--- rScraper/rScraperYoutube.py
+++
@@ -1,6 +0,0 @@
-from rScraper.rScraper import Scraper
-
-
-class ScraperYoutube(Scraper):
-    def __init__(self):
-        pass
--- rScraper/rYtWebParser.py
+++
@@ -1,4 +0,0 @@
-# Youtube web interface interface parser
-class YoutubeRs(object):
-    def __init__(self):
-        pass
