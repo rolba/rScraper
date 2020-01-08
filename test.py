f9540c2478b3e544782b084ae0c58b0f7ab4a352
Pawel Rolbiecki <pawel.rolbiecki@gmail.com>
2020-01-08 22:53:21 +0100

    Clearing ScraperGoogle constructor parameters.

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ from rScraper.rScraperGoogle import ScraperGoogle
 import os
 def main():
     scrapingItem = "dog"
-    googleScraper = ScraperGoogle(scrapingItem, directory= os.getcwd() + "/Downloads", delOldDwldDir=True)
+    scrapingDir = os.getcwd() + "/Downloads"
+    googleScraper = ScraperGoogle(scrapingItem, directory=scrapingDir, delOldDwldDir=True)
     googleScraper.openBrowser()
     googleScraper.searchPhrase()
     googleScraper.downloadImages()
